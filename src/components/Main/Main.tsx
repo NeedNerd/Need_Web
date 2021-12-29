f60d76e451bfc332364bdc56d45ed163b23d8fdf
leeminewook <63051680+leeminewook@users.noreply.github.com>
2021-12-29 18:54:21 +0900

    [ADD] 메인 페이지

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -10,11 +10,11 @@ const Main = () => {
           <div className="MainForm-AllMyTag-My">MY</div>
         </div>
         <div className="MainForm-SubTag">
-          <div className="MainForm-SubTag-item">구매원해요</div>
-          <div className="MainForm-SubTag-item">나눔원해요</div>
-          <div className="MainForm-SubTag-item">이제 괜찮아요</div>
-          <div className="MainForm-SubTag-item">구매했어요</div>
-          <div className="MainForm-SubTag-item">나눔받았어요</div>
+          <div className="MainForm-SubTag-item buy">구매원해요</div>
+          <div className="MainForm-SubTag-item gift">나눔원해요</div>
+          <div className="MainForm-SubTag-item end">이제 괜찮아요</div>
+          <div className="MainForm-SubTag-item buyEnd">구매했어요</div>
+          <div className="MainForm-SubTag-item giftEnd">나눔받았어요</div>
         </div>
         <div className="MainForm-ItemList">
           <Item />
--- src/components/common/Item/Item.tsx
+++ src/components/common/Item/Item.tsx
@@ -1,9 +1,23 @@
 import "./Item.scss";
-
+import img from "../../../assets/img/chicken1.svg";
 const Item = () => {
   return (
     <>
-      <div className="ItemForm">item</div>
+      <div className="ItemForm">
+        <div className="ItemForm-ImgDiv">
+          <img src={img} alt="" className="ItemForm-ImgDiv-Img" />
+        </div>
+        <div className="ItemForm-TextDiv">
+          <div className="ItemForm-TextDiv-TitleTag">
+            <div className="ItemForm-TextDiv-TitleTag-Title">치킨 삽니다</div>
+            <div className="ItemForm-TextDiv-TitleTag-Tag">구매원해요</div>
+          </div>
+          <div className="ItemForm-TextDiv-NameTime">
+            <div className="ItemForm-TextDiv-NameTime-Name">사승은</div>
+            <div className="ItemForm-TextDiv-NameTime-Time">2021/01/13</div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
