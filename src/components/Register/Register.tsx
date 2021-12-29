8dcf3ce0d2a077e92bac29618a0a44a2f20c63fb
leeminewook <63051680+leeminewook@users.noreply.github.com>
2021-12-29 20:35:42 +0900

    [ADD]회원 가입 페이지

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -1,10 +1,55 @@
 import react from "react";
 import "./Register.scss";
+import logo from "../../assets/img/Group 17 2.svg";
 
 const Register = () => {
   return (
     <>
-      <div className="RegisterForm">register</div>
+      <div className="RegisterForm">
+        <div className="RegisterForm-Logo">
+          <img src={logo} alt="" className="RegisterForm-Logo-Img" />
+        </div>
+        <div className="RegisterForm-Options">
+          <div className="RegisterForm-Options-id">
+            <input
+              type="text"
+              className="RegisterForm-Options-id-input AuthInput"
+              placeholder="아이디"
+            />
+          </div>
+          <div className="RegisterForm-Options-pw">
+            <input
+              type="text"
+              className="RegisterForm-Options-pw-input AuthInput"
+              placeholder="비밀번호"
+            />
+          </div>
+          <div className="RegisterForm-Options-name">
+            <input
+              type="text"
+              className="RegisterForm-Options-name-input AuthInput"
+              placeholder="이름"
+            />
+          </div>
+          <div className="RegisterForm-Options-phoneNum">
+            <input
+              type="text"
+              className="RegisterForm-Options-phoneNum-input AuthInput"
+              placeholder="전화번호"
+            />
+          </div>
+          <div className="RegisterForm-Options-area">
+            <input
+              type="text"
+              className="RegisterForm-Options-area-input AuthInput"
+              placeholder="거주 지역"
+            />
+          </div>
+        </div>
+        <div className="RegisterForm-Submit">
+          <button className="RegisterForm-Submit-btn">가입 완료</button>
+        </div>
+      </div>
     </>
   );
 };
