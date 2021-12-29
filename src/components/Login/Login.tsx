152ae000964f8b65aad6de4b0fa2ea0fd178b033
leeminewook <63051680+leeminewook@users.noreply.github.com>
2021-12-29 18:54:35 +0900

    [ADD] 로그인 페이지

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,10 +1,45 @@
 import react from "react";
 import "./Login.scss";
+import logo from "../../assets/img/Group 17 2.svg";
 
 const Login = () => {
   return (
     <>
-      <div className="LoginForm">login</div>
+      <div className="LoginForm">
+        <div className="LoginForm-Logo">
+          <img src={logo} alt="" className="LoginForm-Logo-Img" />
+        </div>
+        <div className="LoginForm-Options">
+          <div className="LoginForm-Options-Inputs">
+            <div className="LoginForm-Options-Inputs-Id">
+              <input
+                type="text"
+                className="LoginForm-Options-Inputs-Id-input"
+                placeholder="아이디"
+              />
+            </div>
+            <div className="LoginForm-Options-Inputs-Pw">
+              <input
+                type="text"
+                className="LoginForm-Options-Inputs-Pw-input"
+                placeholder="비밀번호"
+              />
+            </div>
+          </div>
+          <div className="LoginForm-Options-Submits">
+            <div className="LoginForm-Options-Submits-Login">
+              <button className="LoginForm-Options-Submits-Login-btn">
+                로그인
+              </button>
+            </div>
+            <div className="LoginForm-Options-Submits-Register">
+              <button className="LoginForm-Options-Submits-Register-btn">
+                회원가입
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
